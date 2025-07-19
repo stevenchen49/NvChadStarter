b21dc2a6821df867d5afdb8a11854be041b5ab24
stevenchen49 <stevenchen49@163.com>
2025-07-20 01:09:48 +0800

    feat(plugins): add new plugin 'stevearc/aerial.nvim'

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -35,5 +35,35 @@ return {
         disable = { "rust" },
       }
     },
+
+    {
+      'stevearc/aerial.nvim',
+      opts = {},
+      -- Optional dependencies
+      dependencies = {
+         "nvim-treesitter/nvim-treesitter",
+         "nvim-tree/nvim-web-devicons"
+      },
+      lazy = false,
+      config = function()
+        require("aerial").setup({
+          -- Priority list of preferred backends for aerial.
+          -- This can be a filetype map (see :help aerial-filetype-map)
+          backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
+
+          -- Determines the default direction to open the aerial window. The 'prefer'
+          -- options will open the window in the other direction *if* there is a
+          -- different buffer in the way of the preferred direction
+          -- Enum: prefer_right, prefer_left, right, left, float
+          default_direction = "prefer_left",
+
+          on_attach = function(bufnr)
+            -- Jump forwards/backwards with '{' and '}'
+            vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+            vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+          end
+        })
+      end
+    },
   },
 }
