13d993128c015bcc96b0e7c39ca5d78963edee2d
stevenchen49 <stevenchen49@163.com>
2025-07-20 00:36:58 +0800

    feat(plugins): setup nvim-treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,13 +16,24 @@ return {
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash", "c", "cpp", "cmake", "gitcommit",
+        "json", "kconfig", "lua", "markdown",
+        "python", "vim", "vimdoc",
+      },
+
+      highlight = {
+        enable = true,
+
+        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+        -- the name of the parser)
+        -- list of language that will be disabled
+        disable = { "rust" },
+      }
+    },
+  },
 }
