a63aed6c2c37b76567add985b58cee9e84b2e0e5
stevenchen49 <stevenchen49@163.com>
2025-07-20 00:14:30 +0800

    feat(lspconfig): enable some common lsp servers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,14 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = {
+    "lua_ls",
+    "cmake",
+    "clangd",
+    "bashls",
+    "pyright",
+    "json"
+}
+
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
